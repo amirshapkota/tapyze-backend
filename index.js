80df76bc33f912429f244e56a1a39cad99655f9b
amirshapkota <shapkotaamir0@gmail.com>
2025-05-09 23:32:47 +0545

    changed to modules

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+// simple route
+app.get('/', (req, res) => {    
+  res.send('Hello World!');
+}
+);
+// start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+}
+);
